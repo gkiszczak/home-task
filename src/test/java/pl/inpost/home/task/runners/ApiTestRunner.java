811d8e3f59b4cf9586647a3a019997d7fb29fa98
Grzegorz Kiszczak <gkiszczak@gmail.com>
2025-02-16 00:58:48 +0100

    fixed compilation error

--- src/test/java/pl/inpost/home/task/runners/ApiTestRunner.java
+++ src/test/java/pl/inpost/home/task/runners/ApiTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/api_test.features",
-        glue = "pl.inpost.home.task.api/steps",
+        glue = "pl.inpost.home.task.api.steps",
         plugin = {"pretty", "html:target/reports/cucumber-api.html"}
 )
 public class ApiTestRunner {
