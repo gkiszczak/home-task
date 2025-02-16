b9ca7ccb5fac37cf44b69355c15abaed88a9adfa
Grzegorz Kiszczak <gkiszczak@gmail.com>
2025-02-16 15:07:09 +0100

    added ignore unknown

--- src/test/java/pl/inpost/home/task/api/model/AddressDetails.java
+++ src/test/java/pl/inpost/home/task/api/model/AddressDetails.java
@@ -1,7 +1,10 @@
 package pl.inpost.home.task.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AddressDetails {
 
     public String getPostCode() {
