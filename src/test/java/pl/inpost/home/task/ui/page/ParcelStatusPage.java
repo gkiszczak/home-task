5b5107cbfd8c070979e18d6f34d2d3df008bcbfc
Grzegorz Kiszczak <gkiszczak@gmail.com>
2025-02-18 10:43:41 +0100

    fix error

--- src/test/java/pl/inpost/home/task/ui/page/ParcelStatusPage.java
+++ src/test/java/pl/inpost/home/task/ui/page/ParcelStatusPage.java
@@ -1,6 +1,6 @@
 package pl.inpost.home.task.ui.page;
 
-import 
+import pl.inpost.home.task.ui.page.annotations.PageUrl;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
