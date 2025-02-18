14497334e7eae290f6394d017e82ba8e2dda4b61
Grzegorz Kiszczak <gkiszczak@gmail.com>
2025-02-18 13:04:10 +0100

    updated api tests

--- src/test/java/pl/inpost/home/task/api/steps/ApiStepDefinitions.java
+++ src/test/java/pl/inpost/home/task/api/steps/ApiStepDefinitions.java
@@ -7,6 +7,8 @@ import org.assertj.core.api.Assertions;
 import pl.inpost.home.task.api.helpers.ParcelLockersHelper;
 import pl.inpost.home.task.api.model.Point;
 
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 
 public class ApiStepDefinitions {
@@ -15,19 +17,32 @@ public class ApiStepDefinitions {
     private ParcelLockersHelper parcelLockersHelper = new ParcelLockersHelper();
 
     private List<Point> points;
+    private Map<String, List<Point>> cityPointsMap = new HashMap<>();
 
     @Given("I fetch all points in {string}")
-    public void iFetchAllPointsIn(String city) {
-        points = parcelLockersHelper.getParcelLockersForCity(city);
+    public void iFetchAllPointsIn(String cities) {
+        String[] cityArray = cities.split(",");
+        for (String city : cities) {
+            List<Point> cityPoints = parcelLockersHelper.getParcelLockersForCity(city);
+            cityPointsMap.put(city, cityPoints);
+        }
     }
 
     @Then("the response contains at least 1 point")
     public void theResponseContainsAtLeastOnePoint() {
+        for (Map.Entry<String, List<Point>> entry : cityPointsMap.entrySet()) { 
+        String city = entry.getKey();
+        List<Point> points = entry.getValue();
         Assertions.assertThat(points).isNotEmpty();
+        }
     }
 
-    @Then("the response is saved to file with {string} in the name")
-    public void theResponseIsSavedToFile(String city) { 
-        responseFileWriter.savePointsToFile(city, points);
+    @Then("the response is saved to file with city in the name")
+    public void theResponseIsSavedToFile() { 
+          for (Map.Entry<String, List<Point>> entry : cityPointsMap.entrySet()) {
+            String city = entry.getKey();
+            List<Point> points = entry.getValue();
+            responseFileWriter.savePointsToFile(city, points);
+        }
     }
 }
--- src/test/resources/api_tests.feature
+++ src/test/resources/api_tests.feature
@@ -3,5 +3,5 @@ Feature: InPost API - Search Parcel Lockers
   Scenario: Verify all InPost points in Gdynia can be retrieved
     Given I fetch all points in "Gdynia"
     Then the response contains at least 1 point
-    Then the response is saved to file with "Gdynia" in the name
+    Then the response is saved to file with city in the name
     
