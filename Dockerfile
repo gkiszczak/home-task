7e2eaa26c7277136cafd1b80e937389804ce5dfe
Grzegorz Kiszczak <gkiszczak@gmail.com>
2025-02-19 01:08:47 +0100

    uodated scripts

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@ FROM maven:3.8.7-eclipse-temurin-17
 WORKDIR /app
 COPY . .
 
-RUN mvn clean install
-
 COPY run-tests.sh /run-tests.sh
 RUN chmod +x /run-tests.sh
 
--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,7 @@ echo "Running tests of type: $TEST_TYPE"
 if [ "$TEST_TYPE" = "UI" ]; then
     mvn test -Dtest=pl.inpost.home.task.runners.UiTestRunner -Dselenium.remote.url="$SELENIUM_REMOTE_URL" -ntp
 elif [ "$TEST_TYPE" = "API" ]; then
-    mvn test -Dtest=pl.inpost.home.task.runners.ApiTestRunner
+    mvn test -Dtest=pl.inpost.home.task.runners.ApiTestRunner -ntp
 else
-    mvn test  # Runs all tests
+    mvn test  -ntp # Runs all tests
 fi
