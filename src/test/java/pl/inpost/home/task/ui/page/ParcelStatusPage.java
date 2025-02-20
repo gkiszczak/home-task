bb88cbb548ac39416f1e92949e7ff0d3dc93584c
Grzegorz Kiszczak <gkiszczak@gmail.com>
2025-02-20 07:32:12 +0100

    fixed compilation error

--- src/test/java/pl/inpost/home/task/ui/page/ParcelStatusPage.java
+++ src/test/java/pl/inpost/home/task/ui/page/ParcelStatusPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.Keys;
 import java.time.Duration;
 
 @PageUrl("/find-parcel")
